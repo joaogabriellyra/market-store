2f4307eecf5f47192a05804855c922b660e4f031
João Gabriel Lyra <jgabriellyra@hotmail.com>
2022-04-26 17:25:58 -0300

    requisito 3 concluído

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Search extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class Search extends React.Component {
           <form action="">
             <input type="text" />
           </form>
+          <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         </div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
