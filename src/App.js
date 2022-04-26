4365469084059967c7389c9e1bdb7477e85ef038
João Gabriel Lyra <jgabriellyra@hotmail.com>
2022-04-26 15:47:34 -0300

    requisito 1 concluído

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { getCategories } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const result = await getCategories();
+    this.setState({ categories: result });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div />
+    );
+  }
 }
 
 export default App;
